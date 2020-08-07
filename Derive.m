c919c29d9f2f5ecd008dc3d44b5a3faac8c787d6
isma-yod <61437282+isma-yod@users.noreply.github.com>
2020-08-08 01:27:36 +0200

    Update Derive.m

--- Derive.m
+++ Derive.m
@@ -2,3 +2,4 @@ function [djamel]=Derive(k,h1,h2)
 
 djamel=(k*abs(h1 - h2)^(3/2))/(h1 - h2)^2 - (3*k*sign(h1 - h2)*abs(h1 - h2)^(1/2))/(2*(h1 - h2));
 end
+% Cette fonction permettra de calculer la dérivée partielle par rapport à h1,h2,h3,h4,...h9
