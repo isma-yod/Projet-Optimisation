fbdc27db6620cc868d7d595e590014999644ed83
isma-yod <61437282+isma-yod@users.noreply.github.com>
2020-08-10 22:53:01 +0200

    Update Aqueduc.m

--- Aqueduc.m
+++ Aqueduc.m
@@ -1,15 +1,30 @@
 clear all;
 clc;
-augment=linearisation(9,K,C,h);% fct est la matrice augmentÈe du syteme linÈaire a rÈsoudre
-sigma=Gauss_Jordan(augment);  % sigma est la solution du systeme d'Èquation
-n_iterations=10000;
+matrice_augmente=linearisation(9,K,C,h);% matrice_augmente est la matrice augment√©e du syteme 
+                                        %lin√©aire a r√©soudre
+sigma=Gauss_Jordan(matrice_augmente);  % sigma est la solution du systeme d'√©quation
 
-for i=1:n_iterations
-    x(i)=sigma+h;
+h1 = sigma+h;                 % h1 est la est la valeur du vecteur 
+                              % √† l'it√©ration 1                                       
+
+
+j=1; % Initialisation du compteur
+while(j < 2)
+    matrice_augmente=linearisation(9,K,C,h);% matrice_augmente est la matrice augment√©e du syteme 
+                                        %lin√©aire a r√©soudre
+    sigma=Gauss_Jordan(matrice_augmente);  % sigma est la solution du systeme d'√©quation
+
+    h = sigma+h;                  % h_n_plus_un est la est la valeur du vecteur 
+                                            % √† l'it√©ration 1                                       
+    j=j+1;
 end
+disp(h)
+disp(j)
+
+
 [Diametre,L,n_manning,A,Rh,K,C,h,Cfeu]= lecture_Donnees();
 
-n=length(C);    %Nombre de noeuds. C correspond au dÈbit transitant par 
+n=length(C);    %Nombre de noeuds. C correspond au d√©bit transitant par 
                 %chaque noeud (Attention!! C est en m^3/s)
 p=length(K);    %Nombre de conduites.
 
@@ -19,17 +34,17 @@ p=length(K);    %Nombre de conduites.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%Vous devez ici effectuer la sÈquence d'itÈrations permettant de calculer
-%les dÈbits des conduites et la charge dans chaque noeud.
+%Vous devez ici effectuer la s√©quence d'it√©rations permettant de calculer
+%les d√©bits des conduites et la charge dans chaque noeud.
 
-%Vous avez les fonctions Q.m, debit.m, fonction.m, lecture_Donnees.m  ‡ votre
+%Vous avez les fonctions Q.m, debit.m, fonction.m, lecture_Donnees.m  √† votre
 %disposition
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
 
-%Partie affichage des rÈsultats. Vous aurez peut-Ítre ‡ changer certains
-%paramËtres si vous changez les noms de variables
+%Partie affichage des r√©sultats. Vous aurez peut-√™tre √† changer certains
+%param√®tres si vous changez les noms de variables
 disp(['La precision obtenue est '  num2str(erreur)])
 disp(['Le nombre d''iteration(s) effectuee(s) est de ' num2str(n_iterations)])
 
@@ -43,7 +58,7 @@ Resultats_charge_noeuds(1,2) =100;
 
 for i=1:n
     Resultats_charge_noeuds(i+1,2) = h(i);  %Note : le vecteur h correspond 
-                                                    %‡ la charge aux noeuds
+                                                    %√† la charge aux noeuds
 end
 
 Resultats_debits_conduite
